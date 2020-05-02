0264ebea582c961e52fc609e5f9b7ccde49223c1
g-bulgarit <alonio6@gmail.com>
2020-05-02 12:10:52 +0300

    added first functions to do gcode conversions

---
+++ Generator/builder_functions.py
@@ -0,0 +1,38 @@
+# File to hold all functions to generate motion based on the weave pattern.
+
+import numpy as np
+
+# Get constants
+peg_diameter = 455
+num_pegs = 120
+peg_to_degree_ratio = 360/num_pegs
+motor_max_travel = peg_diameter * np.pi  # Full revolution of the frame.
+
+def move_from_to(motor, from_pos, to_pos):
+    # Takes in <from> and <to> peg numbers, decides on best way to go,
+    # calculates the conversion from radial distance to linear distance
+    # generates and returns gcode line.
+
+    direction = 1  # Generally.
+
+    delta_pegs = to_pos - from_pos
+    if delta_pegs < 0:
+        direction = -1
+    # check if it's better to go backwards
+    if delta_pegs > num_pegs / 2:
+        direction = -1
+        distance_to_move = np.abs(delta_pegs - num_pegs/2)
+
+    # find the center angle
+    center_angle = delta_pegs * peg_to_degree_ratio
+    linear_distance = center_angle * (np.pi/180) * peg_diameter
+    # Create gcode string
+    return f"G0 {motor}{linear_distance:.2f}"
+
+def weave_peg():
+    pass
+
+
+
+if __name__ == "__main__":
+    print(move_from_to("x",30,40))
