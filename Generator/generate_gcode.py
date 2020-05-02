863098993d2f05ae4f29378cbae225f9aa16e511
g-bulgarit <alonio6@gmail.com>
2020-05-02 19:45:04 +0300

    generator now works, have not tested yet though

--- Generator/generate_gcode.py
+++ Generator/generate_gcode.py
@@ -1,16 +1,23 @@
 from Generator.builder_functions import *
 
 # Open weave file
-weave_file = open("weave.wv", "r'")
+weave_file = "../Simulation/assets/moon_s120_500.wv"
+cfg, pattern = parse_weave_file_wrapper(weave_file)
+lst_pattern = pattern.split(",")
 # get location of the output file
 path_to_gcode = "test.gcode"
 gcode_file = generate_base_gcode(path_to_gcode)
 
 # iterate over weave pattern, generating move and weave commands
 try:
-    pass
-except:
-    pass
+    previous_move = 0
+    move_ctr = 0
+    while move_ctr < len(lst_pattern):
+        gcode_file.write(move_from_to(cfg, "x", previous_move, int(lst_pattern[move_ctr])))
+        # do weave
+        previous = int(lst_pattern[move_ctr])
+        move_ctr +=1
+except Exception as e:
+    print(e)
 finally:
-    weave_file.close()
     gcode_file.close()
