dc02a0dbbae291b6f842744a7b41c4616e243819
g-bulgarit <alonio6@gmail.com>
2020-05-02 19:44:49 +0300

    gcode builder now takes params straight from the weave file

--- Generator/builder_functions.py
+++ Generator/builder_functions.py
@@ -1,21 +1,33 @@
 # File to hold all functions to generate motion based on the weave pattern.
-
 import numpy as np
+from Simulation.image_from_pattern import parse_file
 
-# Get constants
-peg_diameter = 455
-num_pegs = 120
-peg_to_degree_ratio = 360/num_pegs
-motor_max_travel = peg_diameter * np.pi  # Full revolution of the frame.
+cfg = {
+        "num_pegs" : 0,
+        "peg_radius" : 0,
+        "peg_to_degree_ratio" : 0,
+        "motor_max_travel" : 0,
+      }
+
+def parse_weave_file_wrapper(file_path):
+    num_pegs, _, _, _, peg_diameter, pattern = parse_file(file_path)
+    cfg["num_pegs"] = num_pegs
+    cfg["peg_radius"] = peg_diameter/2
+    cfg["peg_to_degree_ratio"] = 360/num_pegs
+    cfg["motor_max_travel"] = peg_diameter * np.pi  # Full revolution of the frame.
+    return cfg, pattern
 
 def generate_base_gcode(file_path):
     # generate the gcode file with some boilerplate.
+    # returns file handler for the open file!
+
     gcode_file = open(file_path, "w+")
-    gcode_file.write("G91\n")
-    # add more boilerplate here
+    gcode_file.write(";START: Boilerplate code from the gcode generator\n")
+    gcode_file.write("G91 ;Set all axis to relative motion, as it is easier to work with.\n")
+    gcode_file.write(";END: Boilerplate code from the gcode generator\n")
     return gcode_file
 
-def move_from_to(motor, from_pos, to_pos):
+def move_from_to(cfg, motor, from_pos, to_pos):
 
     # Call G91 first!
     #
@@ -30,15 +42,15 @@ def move_from_to(motor, from_pos, to_pos):
     if delta_pegs < 0:
         direction = -1
     # check if it's better to go backwards
-    if delta_pegs > num_pegs / 2:
+    if delta_pegs > cfg["num_pegs"] / 2:
         direction = -1
-        distance_to_move = np.abs(delta_pegs - num_pegs/2)
+        distance_to_move = np.abs(delta_pegs - cfg["num_pegs"]/2)
 
     # find the center angle
-    center_angle = delta_pegs * peg_to_degree_ratio
-    linear_distance = center_angle * (np.pi/180) * peg_diameter
+    center_angle = delta_pegs * cfg["peg_to_degree_ratio"]
+    linear_distance = center_angle * (np.pi/180) * cfg["peg_radius"]
     # Create gcode string
-    return f"G0 {motor}{linear_distance:.2f}"
+    return f"G0 {motor.upper()}{linear_distance:.3f}\n"
 
 def weave_peg():
     pass
@@ -46,4 +58,8 @@ def weave_peg():
 
 
 if __name__ == "__main__":
-    print(move_from_to("x",40,30))
+    movelist = [30,60,30,120, 80, 60, 20, 0]
+    previous = 0
+    for move in movelist:
+        print(move_from_to("x", previous, move))
+        previous = move
