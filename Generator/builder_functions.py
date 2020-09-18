7d4f48664a891493a5c3bcbae55efdb02976f544
g-bulgarit <alonio6@gmail.com>
2020-09-18 21:58:52 +0300

    Removed a lot of un-needed code

--- Generator/builder_functions.py
+++ Generator/builder_functions.py
@@ -1,10 +1,17 @@
 # File to hold all functions to generate motion based on the weave pattern.
-import numpy as np
 from Simulation.image_from_pattern import parse_file
 from configparser import ConfigParser
 
 
 def parse_weave_file_wrapper(file_path):
+    """
+    Function to fetch weave parameters from the saved output file of the main algo.
+    This information is then used to calculate the motion paths for the motors.
+    :param file_path: Path to the .wv file
+    :return:
+                -cfg: a python dictionary that contains the parameters listed below
+                -pattern: the final weave-pattern [list]
+    """
     num_pegs, _, _, _, peg_diameter, pattern = parse_file(file_path)
 
     wv_cfg = ConfigParser()
@@ -14,129 +21,37 @@ def parse_weave_file_wrapper(file_path):
     cfg["num_pegs"] = num_pegs
     cfg["peg_radius"] = peg_diameter/2
     cfg["steps_per_peg"] = wv_cfg["Steps"]["steps_per_peg"]
-    return cfg, pattern
+    lst_pattern = pattern.split(",")
+
+    return cfg, lst_pattern
+
 
 def generate_base_gcode(file_path):
-    # generate the gcode file with some boilerplate.
-    # returns file handler for the open file!
+    """
+    Function to generate the header for the gCode file.
+    TODO: change functionality with the config file (for other motor drivers...?)
+    :param file_path: path where to create the new gcode file in.
+    :return: an **OPEN** file handler, ready for writing.
+    """
 
     gcode_file = open(file_path, "w+")
+
     gcode_file.write(";START: Boilerplate code from the gcode generator\n")
     gcode_file.write("G4 P1000 ;Wait 1Sec.\n")
     gcode_file.write("M201 X25.0 ;Set x axis acceleration to 25.\n")
-    # gcode_file.write("G0 x3.00 ;Move to 3mm.\n")
-    # gcode_file.write("G4 P1500 ;Wait 1Sec.\n")
-    # gcode_file.write("G0 x0.00 ;Move to 0mm.\n")
     gcode_file.write("G4 P1000 ;Wait 1Sec.\n")
     gcode_file.write("G91 ;Set all axis to relative motion, as it is easier to work with.\n")
     gcode_file.write("G4 P1000 ;Wait 1Sec.\n")
     gcode_file.write(";END: Boilerplate code from the gcode generator\n")
+
     return gcode_file
 
 
-def generate_abs_base_gcode(file_path):
-    # generate the gcode file with some boilerplate.
-    # returns file handler for the open file!
-
-    gcode_file = open(file_path, "w+")
-    gcode_file.write(";START: Boilerplate code from the gcode generator\n")
-    gcode_file.write("M201 x50.0 ;Set x axis acceleration to 50.\n")
-    gcode_file.write("G90 ;Set all axis to absolute motion.\n")
-    gcode_file.write("G4 P1000 ;Wait 1Sec.\n")
-    gcode_file.write(";END: Boilerplate code from the gcode generator\n")
-    return gcode_file
-
-def move_from_to(motor, from_pos, to_pos, debug=False, status=None):
-    # Takes in <from> and <to> peg numbers, decides on best way to go,
-    # calculates the conversion from radial distance to linear distance
-    # generates and returns gcode line.
-
-    direction = 1  # Generally.
-    outstr = ""
-    one_peg_distance = 3.21
-    delta_pegs = to_pos - from_pos
-    if abs(delta_pegs) >= 25:
-        delta_pegs = (50-abs(delta_pegs))
-
-    linear_distance_after_gear = delta_pegs * one_peg_distance
-
-    if status: outstr+= f"M117 {status}\n"
-    if debug: outstr+= f";START: Move to peg {to_pos}\n"
-    outstr += f"G0 {motor.upper()}{linear_distance_after_gear:.3f}\n"
-    if debug: outstr += f";END: Move to peg {to_pos}\n"
-    return outstr
-
-def move_weavehead(dir):
-    if dir == 1:
+def move_weavehead(direction):
+    if direction == 1:
         return "G0 Y-10.00\nG4 P500\n"
     else:
         return "G0 Y10.00\nG4 P500\n"
-
-def move_weavehead_abs(dir):
-    if dir == 1:
-        return "G0 Y-5.00\nG4 P500\n"
-    else:
-        return "G0 Y5.00\nG4 P500\n"
-
-def weave_peg(debug=False):
-    # Calls the weave sequence:
-    #   1. rotate frame <x> degrees to + dir
-    #   2. half turn on the weave motor
-    #   3. rotate frame <2x> degrees to - dir
-    #   4. half turn on the weave motor
-    #   5. rotate frame <x> degree to + dir
-
-    # center_angle = 0.5 * cfg["peg_to_degree_ratio"]
-    # circum = 2 * np.pi * cfg["peg_radius"]
-    # linear_distance = circum / center_angle
-    # half_step = linear_distance / 2
-    # half_step = center_angle * (np.pi/180) * cfg["peg_radius"]
-
-    one_peg_distance = 3.21
-    half_step = one_peg_distance*0.45
-
-    output_block = ""
-    if debug: output_block += ";WEAVE: start\n"
-    output_block += rotate_frame(-half_step)
-    output_block += "G4 P500\n"
-    output_block += move_weavehead(1)
-    output_block += rotate_frame(2* half_step)
-    output_block += "G4 P500\n"
-    output_block += move_weavehead(0)
-    output_block += rotate_frame(-half_step*1.25)
-    output_block += "G4 P500\n"
-    if debug: output_block += ";WEAVE: end\n"
-    return output_block
-
-def weave_peg_absolute(current_loc, debug=False):
-    # Calls the weave sequence:
-    #   1. rotate frame <x> degrees to + dir
-    #   2. half turn on the weave motor
-    #   3. rotate frame <2x> degrees to - dir
-    #   4. half turn on the weave motor
-    #   5. rotate frame <x> degree to + dir
-
-    # center_angle = 0.5 * cfg["peg_to_degree_ratio"]
-    # circum = 2 * np.pi * cfg["peg_radius"]
-    # linear_distance = circum / center_angle
-    # half_step = linear_distance / 2
-    # half_step = center_angle * (np.pi/180) * cfg["peg_radius"]
-
-    one_peg_distance = 3.21
-    half_step = one_peg_distance*0.35
-
-    output_block = ""
-    if debug: output_block += ";WEAVE: start\n"
-    output_block += f"G0 X{str(round(current_loc - half_step,3))}\n"
-    output_block += "G4 P500\n"
-    output_block += move_weavehead_abs(1)
-    output_block += f"G0 X{str(round(current_loc + 2*half_step,3))}\n"
-    output_block += "G4 P500\n"
-    output_block += move_weavehead_abs(0)
-    output_block += f"G0 X{str(round(current_loc - half_step,3))}\n"
-    output_block += "G4 P500\n"
-    if debug: output_block += ";WEAVE: end\n"
-    return output_block
 
 
 def weave_peg_anti_backlash(cfg=None, debug=False):
@@ -148,30 +63,20 @@ def weave_peg_anti_backlash(cfg=None, debug=False):
     # Text container:
     output_block = ""
 
-    if debug: output_block += ";WEAVE[ANTIBACKLASH]: start\n"
+    if debug:
+        output_block += ";WEAVE[ANTIBACKLASH]: start\n"
     output_block += move_weavehead(1)
     output_block += rotate_frame(one_pin_distance)
     output_block += "G4 P500\n"
     output_block += move_weavehead(0)
     output_block += "G4 P500\n"
-    if debug: output_block += ";WEAVE[ANTIBACKLASH]: end\n"
+    if debug:
+        output_block += ";WEAVE[ANTIBACKLASH]: end\n"
 
     return output_block
 
 
-def generate_absolute_gcode(gcode_file, pattern):
-    num_pegs = 50
-    distance_p2p = 3.21
-    pos_dict = {}
-    for i in range(num_pegs-1):
-        pos_dict[i] = i*distance_p2p
-
-    for move in pattern:
-        gcode_file.write(f"G0 X{str(round(pos_dict[int(move)],3))}\n")
-        gcode_file.write(weave_peg_absolute(pos_dict[int(move)]))
-
-
-def move_p2p_one_dir(previous, next, first=False, cfg=None):
+def move_p2p_one_dir(previous_pt, next_pt, first=False, cfg=None):
     if cfg is not None:
         # Take values from config
         one_pin_distance = float(cfg["steps_per_peg"])
@@ -179,28 +84,21 @@ def move_p2p_one_dir(previous, next, first=False, cfg=None):
     else:
         raise Exception("Missing configuration file.")
 
-    if int(next) < int(previous):
-        delta = (num_pegs - int(previous)) + int(next)
+    if int(next_pt) < int(previous_pt):
+        delta = (num_pegs - int(previous_pt)) + int(next_pt)
     else:
-        delta = int(next) - int(previous)
+        delta = int(next_pt) - int(previous_pt)
 
-    distance_to_move = round(one_pin_distance * delta ,3)
+    distance_to_move = round(one_pin_distance * delta, 3)
     if first:
         distance_to_move -= one_pin_distance/2
 
     return f"G0 X{str(distance_to_move)}\n"
 
+
 def rotate_frame(amount):
     outstr = f"G0 X{round(amount,3)}\n"
     return outstr
-
-def generate_relative_gcode(gcode_file, pattern):
-    current_move = 0
-
-    for next_move in pattern:
-        gcode_file.write(move_p2p_one_dir(current_move, next_move))
-        gcode_file.write(weave_peg())
-        current_move = next_move
 
 
 def generate_antibacklash_gcode(gcode_file, pattern, cfg):
@@ -214,21 +112,6 @@ def generate_antibacklash_gcode(gcode_file, pattern, cfg):
         current_move = next_move
 
 
-
-
-
 if __name__ == "__main__":
-    # some test lines
-
-    cfg = {
-        "num_pegs": 49,
-        "peg_radius": 82,
-        "peg_to_degree_ratio": 7.2,
-    }
-
-    movelist = [5,10,5,10, 0, 24, 0, 5]
-    previous = 0
-    for move in movelist:
-        print(f"Moving to {move}")
-        print(move_from_to(cfg, "x", previous, move))
-        previous = move
+    # Do nothing
+    pass
--- Generator/generate_gcode.py
+++ Generator/generate_gcode.py
@@ -3,14 +3,13 @@ from Generator.builder_functions import *
 # Load files and parse data
 weave_file = r"G:\Code\Weaver\Simulation\assets\cat_s180_10000.wv"
 cfg, pattern = parse_weave_file_wrapper(weave_file)
-lst_pattern = pattern.split(",")
 
 # create the output file
 path_to_gcode = "out.gcode"
 gcode_file = generate_base_gcode(path_to_gcode)
 
 # iterate over weave pattern, generating move and weave commands
-generate_antibacklash_gcode(gcode_file, lst_pattern, cfg)
+generate_antibacklash_gcode(gcode_file, pattern, cfg)
 
 gcode_file.write("M117 100%\n")
 
